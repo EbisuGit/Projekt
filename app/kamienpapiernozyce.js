7935f6a6bd7b37d68a7320e15b2c534eb808f143
bertoven <hubertwlk@gmail.com>
2017-05-30 23:32:20 +0200

    added impl

--- app/kamienpapiernozyce.js
+++ app/kamienpapiernozyce.js
@@ -1,3 +1,42 @@
-/**
- * Created by Ebisu on 5/30/2017.
- */
+
+switch(wyborGracza1) {
+    case kamien:
+        switch(wyborGracza1) {
+            case kamien:
+                return "remis"
+                break;
+            case papier:
+                return "gracz2"
+                break;
+            case nozyce:
+                return "gracz1"
+                break;
+        }
+        break;
+    case papier:
+        switch(wyborGracza1) {
+            case kamien:
+                return "gracz1"
+                break;
+            case papier:
+                return "remis"
+                break;
+            case nozyce:
+                return "gracz2"
+                break;
+        }
+        break;
+    case nozyce:
+        switch(wyborGracza1) {
+            case kamien:
+                return "gracz2"
+                break;
+            case papier:
+                return "gracz1"
+                break;
+            case nozyce:
+                return "remis"
+                break;
+        }
+        break;
+}
