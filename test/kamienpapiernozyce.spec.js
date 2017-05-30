715bf680b005a9e6cfc23fc2c95e7b92bb904bf1
EbisuDev <ebisu.mobile@gmail.com>
2017-05-31 01:20:00 +0200

    added error unit tests

--- test/kamienpapiernozyce.spec.js
+++ test/kamienpapiernozyce.spec.js
@@ -2,43 +2,61 @@
 var expect = require('chai').expect;
 var app = require('../app/kamienpapiernozyce.js');
 
-describe('outcomes', function ()
+describe('unit tests', function()
 {
-    it('should return remis', function ()
+    describe('outcomes', function ()
     {
-        expect(app.kamienPapierNozyce("kamien", "kamien")).to.eql("remis");
-    });
-    it('should return gracz2', function ()
-    {
-        expect(app.kamienPapierNozyce("kamien", "papier")).to.eql("gracz2");
-    });
-    it('should return gracz1', function ()
-    {
-        expect(app.kamienPapierNozyce("kamien", "nozyce")).to.eql("gracz1");
-    });
-    it('should return gracz1', function ()
-    {
-        expect(app.kamienPapierNozyce("papier", "kamien")).to.eql("gracz1");
-    });
-    it('should return remis', function ()
-    {
-        expect(app.kamienPapierNozyce("papier", "papier")).to.eql("remis");
-    });
-    it('should return gracz2', function ()
-    {
-        expect(app.kamienPapierNozyce("papier", "nozyce")).to.eql("gracz2");
-    });
-    it('should return gracz2', function ()
-    {
-        expect(app.kamienPapierNozyce("nozyce", "kamien")).to.eql("gracz2");
-    });
-    it('should return gracz1', function ()
-    {
-        expect(app.kamienPapierNozyce("nozyce", "papier")).to.eql("gracz1");
-    });
-    it('should return remis', function ()
-    {
-        expect(app.kamienPapierNozyce("nozyce", "nozyce")).to.eql("remis");
+        it('should return remis', function ()
+        {
+            expect(app.kamienPapierNozyce("kamien", "kamien")).to.eql("remis");
+        });
+        it('should return gracz2', function ()
+        {
+            expect(app.kamienPapierNozyce("kamien", "papier")).to.eql("gracz2");
+        });
+        it('should return gracz1', function ()
+        {
+            expect(app.kamienPapierNozyce("kamien", "nozyce")).to.eql("gracz1");
+        });
+        it('should return gracz1', function ()
+        {
+            expect(app.kamienPapierNozyce("papier", "kamien")).to.eql("gracz1");
+        });
+        it('should return remis', function ()
+        {
+            expect(app.kamienPapierNozyce("papier", "papier")).to.eql("remis");
+        });
+        it('should return gracz2', function ()
+        {
+            expect(app.kamienPapierNozyce("papier", "nozyce")).to.eql("gracz2");
+        });
+        it('should return gracz2', function ()
+        {
+            expect(app.kamienPapierNozyce("nozyce", "kamien")).to.eql("gracz2");
+        });
+        it('should return gracz1', function ()
+        {
+            expect(app.kamienPapierNozyce("nozyce", "papier")).to.eql("gracz1");
+        });
+        it('should return remis', function ()
+        {
+            expect(app.kamienPapierNozyce("nozyce", "nozyce")).to.eql("remis");
+        });
+
     });
 
-});
+    describe('error cases', function() {
+        it('it should return false', function() {
+            expect(app.kamienPapierNozyce(42, "nozyce")).to.eql(false);
+        })
+        it('it should return false', function() {
+            expect(app.kamienPapierNozyce("kamien", 3.14)).to.eql(false);
+        })
+        it('it should return false', function() {
+            expect(app.kamienPapierNozyce("lol", "lmao")).to.eql(false);
+        })
+        it('it should return false', function() {
+            expect(app.kamienPapierNozyce("kamien", "rotfl")).to.eql(false);
+        })
+    })
+})
