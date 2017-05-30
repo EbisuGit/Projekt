fe9379ea671e4a0594f289b4b17a87b583a211b2
bertoven <hubertwlk@gmail.com>
2017-05-30 23:54:01 +0200

    added impl

--- app/kamienpapiernozyce.js
+++ app/kamienpapiernozyce.js
@@ -6,4 +6,46 @@ var mozliwosci = ["kamien", "papier", "nozyce"];
 
 function kamienPapierNozyce(wyborGracza1, wyborGracza2) {
 
+    switch(wyborGracza1) {
+        case kamien:
+            switch(wyborGracza1) {
+                case kamien:
+                    return "remis"
+                    break;
+                case papier:
+                    return "gracz2"
+                    break;
+                case nozyce:
+                    return "gracz1"
+                    break;
+            }
+            break;
+        case papier:
+            switch(wyborGracza1) {
+                case kamien:
+                    return "gracz1"
+                    break;
+                case papier:
+                    return "remis"
+                    break;
+                case nozyce:
+                    return "gracz2"
+                    break;
+            }
+            break;
+        case nozyce:
+            switch(wyborGracza1) {
+                case kamien:
+                    return "gracz2"
+                    break;
+                case papier:
+                    return "gracz1"
+                    break;
+                case nozyce:
+                    return "remis"
+                    break;
+            }
+            break;
+    }
+
 }
