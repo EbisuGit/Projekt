c508378b4c405769b3836c339dfc79e518483a76
The Almaighty <ebisu.mobile@gmail.com>
2017-05-31 12:09:24 +0200

    ignored npm-debug and applied static-analysis

--- app/kamienpapiernozyce.js
+++ app/kamienpapiernozyce.js
@@ -2,60 +2,63 @@
  * Created by Ebisu on 5/30/2017.
  */
 
-var mozliwosci = ["kamien", "papier", "nozyce"];
+var mozliwosci = ['kamien', 'papier', 'nozyce'];
 
 function kamienPapierNozyce(wyborGracza1, wyborGracza2) {
 
-    if(typeof(wyborGracza1) != "string")
+    if(typeof(wyborGracza1) !== 'string') {
+		    return false;
+    }
+
+    if(typeof(wyborGracza2) !== 'string') {
+		    return false;
+    }
+
+    if((mozliwosci.indexOf(wyborGracza1) > -1) === true) {
         return false;
+    }
 
-    if(typeof(wyborGracza2) != "string")
+    if((mozliwosci.indexOf(wyborGracza2) > -1) === true) {
         return false;
-
-    if(!(mozliwosci.indexOf(wyborGracza1) > -1))
-        return false
-
-    if(!(mozliwosci.indexOf(wyborGracza2) > -1))
-        return false;
-
+    }
 
     switch(wyborGracza1) {
-        case "kamien":
+        case 'kamien':
             switch(wyborGracza2) {
-                case "kamien":
-                    return "remis"
+                case 'kamien':
+                    return 'remis';
                     break;
-                case "papier":
-                    return "gracz2"
+                case 'papier':
+                    return 'gracz2';
                     break;
-                case "nozyce":
-                    return "gracz1"
+                case 'nozyce':
+                    return 'gracz1';
                     break;
             }
             break;
-        case "papier":
+        case 'papier':
             switch(wyborGracza2) {
-                case "kamien":
-                    return "gracz1"
+                case 'kamien':
+                    return 'gracz1';
                     break;
-                case "papier":
-                    return "remis"
+                case 'papier':
+                    return 'remis';
                     break;
-                case "nozyce":
-                    return "gracz2"
+                case 'nozyce':
+                    return 'gracz2';
                     break;
             }
             break;
-        case "nozyce":
+        case 'nozyce':
             switch(wyborGracza2) {
-                case "kamien":
-                    return "gracz2"
+                case 'kamien':
+                    return 'gracz2';
                     break;
-                case "papier":
-                    return "gracz1"
+                case 'papier':
+                    return 'gracz1';
                     break;
-                case "nozyce":
-                    return "remis"
+                case 'nozyce':
+                    return 'remis';
                     break;
             }
             break;
